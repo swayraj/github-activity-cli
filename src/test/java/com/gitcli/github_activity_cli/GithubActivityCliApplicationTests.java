e1a00826e01008909e2d015a721eff252f739364
swayraj <swarajdhayarkar@gmail.com>
2025-10-27 12:01:57 +0530

    Initial Spring Boot Project Setup

---
+++ src/test/java/com/gitcli/github_activity_cli/GithubActivityCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitcli.github_activity_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActivityCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
